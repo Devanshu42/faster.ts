c86dcd3c95611ad2ca827fbab88728be31d4dd19
Devanshu42 <Devanshu42@gmail.com>
2020-08-25 12:55:55 +0530

    contact module routing fixed.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
@@ -12,12 +12,13 @@ import { WelcomeFarmerComponent } from './welcome-farmer/welcome-farmer.componen
 import { SellHistoryComponent } from './sell-history/sell-history.component';
 import { WelcomeBidderComponent } from './welcome-bidder/welcome-bidder.component';
 import { ForgetComponent } from './forget/forget.component';
-import {AboutComponent} from './about/about.component';
+import { AboutComponent} from './about/about.component';
 import { LoginFarmerComponent } from './login-farmer/login-farmer.component';
 import { LoginBidderComponent } from './login-bidder/login-bidder.component';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 import { FarmerRegistrationComponent } from './farmer-registration/farmer-registration.component';
 import { BidderRegistrationComponent } from './bidder-registration/bidder-registration.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 const routes: Routes = [
@@ -37,6 +38,7 @@ const routes: Routes = [
   {path: 'login-farmer', component: LoginFarmerComponent},
   {path: 'login-bidder', component: LoginBidderComponent},
   {path: 'admin-dash', component: AdminDashboardComponent},
+  {path: 'contact', component:ContactComponent},
   {path: '**', component: NotfoundComponent}
 
 ];
