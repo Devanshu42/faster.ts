5b1f12c245121f6c29367653ad815c6e8c61eb80
anmolpanwar <anmolpanwar8@gmail.com>
2020-08-26 13:34:15 +0530

    Login turns logout?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit
+{
   iwillhide=false;
   title = 'SchemeForFarmers';
+  loginout:string;
+  ngOnInit(): void {
+    // this.farmerName=sessionStorage.getItem('FarmerName');
+    if(sessionStorage.getItem("farmerEmail")===null)
+    {
+      this.loginout = "Login";
+    }
+    else{
+      this.loginout = "Logout";
+    }
+  }
 
 }
