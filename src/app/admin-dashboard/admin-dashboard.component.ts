23804955ca5dcbf4dda8ca92f9faed56ad66ebd9
anmolpanwar <anmolpanwar8@gmail.com>
2020-08-28 10:01:23 +0530

    admin logout

--- src/app/admin-dashboard/admin-dashboard.component.ts
+++ src/app/admin-dashboard/admin-dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AppComponent } from '../app.component';
 
 @Component({
   selector: 'app-admin-dashboard',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminDashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private refresh: AppComponent) { }
 
   ngOnInit(): void {
+    this.refresh.ngOnInit();
     
   }
 
